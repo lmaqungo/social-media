91fea876cab1d46363aab4caceab97e894e058e8
luya <91703950+lmaqungo@users.noreply.github.com>
2026-04-01 20:22:46 +0200

    style: 100% width of parent container

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -3,7 +3,7 @@ import { CircularProgress } from '@mui/material'
 
 const LoadingIndicator = () => {
   return (
-        <div className='w-full py-10 flex justify-center' >
+        <div className='w-full h-full py-10 flex justify-center items-center' >
             <CircularProgress size={24} />
         </div>
   )
